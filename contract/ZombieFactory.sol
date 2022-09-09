b56f8f420f73160c189d0c799aa329a44be62655
Toki <55911995+tokio-k@users.noreply.github.com>
2022-09-09 14:33:09 +0900

    complete lesson 2

--- contract/ZombieFactory.sol
+++ contract/ZombieFactory.sol
@@ -13,10 +13,14 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string memory _name, uint256 _dna) private {
+    function _createZombie(string memory _name, uint256 _dna) internal {
         zombies.push(Zombie(_name, _dna));
         uint256 id = zombies.length - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] ++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -30,6 +34,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint256 randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
